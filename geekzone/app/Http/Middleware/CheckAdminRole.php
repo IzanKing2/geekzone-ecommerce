894a2191993ddc42742fe1077b2e1b979f09fbc0
IzanKing2 <izanwork2@gmail.com>
2026-03-11 12:47:15 +0000

    🌱 feat(middleware): añadiendo Middlewares de protección.
       - JwtMiddleware (verifica si el token es válido).
       - CheckAdminRole (verifica que el usuario es administrador).

---
+++ geekzone/app/Http/Middleware/CheckAdminRole.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckAdminRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = Auth::user();
+
+        if (!$user || $user->rol !== 'admin') {
+            return response()->json([
+                'message' => 'Acceso denegado: debes ser administrador para acceder a este recurso',
+            ], 403);
+        }
+
+        return $next($request);
+    }
+}
---
+++ geekzone/app/Http/Middleware/JwtMiddleware.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use PHPOpenSourceSaver\JWTAuth\Exceptions\JWTException;
+use PHPOpenSourceSaver\JWTAuth\Exceptions\TokenExpiredException;
+use PHPOpenSourceSaver\JWTAuth\Exceptions\TokenInvalidException;
+use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
+use Symfony\Component\HttpFoundation\Response;
+
+class JwtMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        try {
+            // Verificamos si el token es válido
+            $user = JWTAuth::parseToken()->authenticate();
+
+            if (!$user) {
+                return response()->json([
+                    'message' => 'Usuario no encontrado',
+                ], 404);
+            }
+        } catch (TokenExpiredException $e) {
+            return response()->json([
+                'message' => 'Token expirado',
+            ], 401);
+        } catch (TokenInvalidException $e) {
+            return response()->json([
+                'message' => 'Token inválido',
+            ], 401);
+        } catch (JWTException $e) {
+            return response()->json([
+                'message' => 'Token no encontrado',
+            ], 401);
+        }
+
+        return $next($request);
+    }
+}
